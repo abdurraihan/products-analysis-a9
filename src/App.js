77b694e27acf1c8be084b244767f9f40afc77024
abdurraihan <abdurraihan898@gmail.com>
2022-04-04 22:24:29 +0600

    after do header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
+import Blogs from './Component/Blogs/Blogs';
 import Header from './Component/Header/Header';
 import Home from './Component/Home/Home';
 import NotFound from './Component/NotFound/NotFound';
+import Dashbord from './Component/Reviews/Dashbord/Dashbord';
+import Reviews from './Component/Reviews/Reviews';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home> </Home>} > </Route>
         <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashbord></Dashbord>} ></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About> </About>}></Route>
 
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+           <nav className='text-3xl flex justify-center  p-4 '>
+              <Link className='mx-5  hover:bg-orange-200 ' to='/home' >Home</Link>
+              <Link  className='mx-5  hover:bg-orange-200 ' to='/reviews' >Reviews</Link>
+              
+              <Link  className='mx-5  hover:bg-orange-200 ' to='/dashboard' >Dashboard</Link>
+              <Link  className='mx-5  hover:bg-orange-200 ' to='blogs' >Blogs</Link>
+              <Link  className='mx-5  hover:bg-orange-200 ' to='/about'>About</Link>
+              
+           </nav>
         </div>
     );
 };
